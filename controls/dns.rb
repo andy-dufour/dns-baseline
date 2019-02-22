66195ebfc69c943014641d94cf8b7fc5d626cf48
Andrew Dufour <adufour@chef.io>
2019-02-21 20:07:47 -0500

    adding trailing newline

--- controls/dns.rb
+++ controls/dns.rb
@@ -1,3 +1,3 @@
 describe command('cat /etc/resolv.conf | grep nameserver') do
-  its('stdout') { should eq "nameserver 1.2.3.4\nnameserver 5.6.7.8" }
+  its('stdout') { should eq "nameserver 1.2.3.4\nnameserver 5.6.7.8\n" }
 end
